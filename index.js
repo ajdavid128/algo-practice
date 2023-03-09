7825e2a4943d361a3fc960b55dff8a292876d901
Aaron David <ajdavid128@gmail.com>
2023-03-09 11:56:26 -0600

    solved problem 2, used ChatGPT for assistance

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ function makeNegative(num) {
 
 // Problem 2:
 
+// My attempt: Incorrect
+function validatePIN (pin) {
+    let test = Math.abs(pin)
+    if ( Number.isInteger(test) === true && pin.split('').length === 4 || pin.split('').length === 6) {
+      return true
+    } else {
+      return false
+    };
+  };
+
+// Form ChatGPT:
+function validatePIN(pin) {
+    if (pin.length === 4 || pin.length === 6) {  // Check the length of the PIN
+      for (let i = 0; i < pin.length; i++) {
+        if (isNaN(parseInt(pin[i]))) {  // Check if each character is a digit
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
