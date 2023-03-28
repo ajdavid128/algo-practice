0716b0d0e7a427dbf3f6a9384f2b15af4dd59bcd
Aaron David <ajdavid128@gmail.com>
2023-03-28 11:28:51 -0500

    added solution for problem 19

--- index.js
+++ index.js
@@ -272,6 +272,19 @@ for (let i = 0; i < 7; i++) {
 }
 
 // Problem 19: -------------------------------------------------------------
-
+//Eloquent JavaScript Solution
+let size = 8
+let board = ""
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y)%2 == 0){
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+console.log(board);
 
 // Problem 20: -------------------------------------------------------------
