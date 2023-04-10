d8969f41652a240e7a1d585999e0e83842e6c328
Aaron David <ajdavid128@gmail.com>
2023-04-10 09:56:41 -0500

    added solution for problem 20

--- index.js
+++ index.js
@@ -288,6 +288,25 @@ for (let y = 0; y < size; y++) {
 console.log(board);
 
 // Problem 20: -------------------------------------------------------------
+let count = 0;
+
+function cc(card) {
+  if (card === 1 || card === 2 || card === 3 || card === 4 || card === 5 || card === 6) {
+    count++;
+  } else if (card === 10 || card === 'J' || card === 'Q' || card === 'K' || card === 'A') {
+    count--;
+  } else {
+    count
+  }
+  if (count > 0) {
+    return count + ' Bet'
+  } else {
+    return count + ' Hold'
+  }
+}
+
+
+
 
 // Problem 21: -------------------------------------------------------------
 function solution(number){
