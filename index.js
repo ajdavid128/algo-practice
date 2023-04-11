e1717964806c2d0bd1c62e63444d6ff23a7e8b36
Aaron David <ajdavid128@gmail.com>
2023-04-11 10:40:52 -0500

    added community solution

--- index.js
+++ index.js
@@ -430,6 +430,24 @@ function updateRecords(records, id, prop, value) {
 }
 
 // Problem 24: -------------------------------------------------------------
+// community solution
+function duplicateCount(text){
+  var lower = text.toLowerCase();
+  var count = 0;
+  var used = [];
+  
+  lower.split('').forEach(function(letter) {
+    if (!used.includes(letter) && (lower.split(letter).length - 1) > 1) {
+      count++;
+      used.push(letter);
+    }
+  });
+  
+  return count;
+}
+
+
+
 // ChatGPT Solution
 function duplicateCount(text){
   let str = text.toLowerCase();
