a0f3ae5a69148c7c93d2bb5a4917a153dd2061f2
Aaron David <ajdavid128@gmail.com>
2023-04-17 15:39:56 -0500

    added information about radix to the NewConcepts file

--- NewConcepts.js
+++ NewConcepts.js
@@ -36,3 +36,25 @@ function sum(arr, n) {
       return sum(arr, n - 1) + arr[n - 1];
     }
   }
+
+  // -------------------------------------------------------------
+
+// Use the parseInt Function with a Radix
+// The parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
+
+// The function call looks like:
+parseInt(string, radix);
+
+// And here's an example:
+const a = parseInt("11", 2);
+
+// The radix variable says that 11 is in the binary system, or base 2. This example converts the string 11 to an integer 3.
+
+// Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+  //using 2 as the radix because it is binary
+}
+
+convertToInteger("10011");
