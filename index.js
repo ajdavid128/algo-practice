7244079903c03cfad78c370f10794c90a94a021b
Aaron David <ajdavid128@gmail.com>
2023-04-20 11:34:06 -0500

    chatgpt solution 46

--- index.js
+++ index.js
@@ -870,6 +870,16 @@ function cockroachSpeed(s) {
 }
 
 // Problem 46: -------------------------------------------------------------
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
+
 
 // Problem 47: -------------------------------------------------------------
 
