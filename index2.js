3fe3923e0ae2dc98878eb4ed453a5b2dc5e20084
Aaron David <ajdavid128@gmail.com>
2023-04-27 20:45:01 -0500

    solved 2 on index 2

--- index2.js
+++ index2.js
@@ -3,3 +3,15 @@ function makeNegative(num) {
     return num == 0 || num < 0 ? num : -num;
 }
 
+// Problem 2: -------------------------------------------------------------
+function validatePIN(str) {
+    if (str.length == 4 || str.length == 6) {
+        if (Math.abs(str)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
