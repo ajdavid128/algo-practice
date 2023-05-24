6d872f0ce4379a2aee0301b7f9f69ea62873cd44
Aaron David <ajdavid128@gmail.com>
2023-05-24 09:24:14 -0500

    added/solved fundamental P10

--- Fundamentals/fundamentals.js
+++ Fundamentals/fundamentals.js
@@ -114,6 +114,13 @@ function findDifference(a, b) {
 }
 
 ////////////////////////// PROBLEM #10 //////////////////////////
+function squareSum(n){
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum += n[i] * n[i]
+  }
+  return sum;
+}
 
 
 ////////////////////////// PROBLEM #11 //////////////////////////
